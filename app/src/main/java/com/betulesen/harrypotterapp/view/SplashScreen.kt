c3d4d50cfe33721b8cc63791d77758406251d6c8
Betul <83249334+betullesen@users.noreply.github.com>
2025-03-06 03:09:20 +0300

    Update SplashScreen.kt

--- app/src/main/java/com/betulesen/harrypotterapp/view/SplashScreen.kt
+++ app/src/main/java/com/betulesen/harrypotterapp/view/SplashScreen.kt
@@ -18,38 +18,38 @@ import com.betulesen.harrypotterapp.R
 @Composable
 fun CharacterSplashScreen(navController: NavController) {
 
-    // Navigasyon işlemi için gecikme ekleyelim
+    
     LaunchedEffect(Unit) {
         Handler(Looper.getMainLooper()).postDelayed({
             navController.navigate("character_list_screen") {
                 popUpTo("splash_screen") { inclusive = true }
             }
-        }, 3000) // Splash ekranı 3 saniye kaldıktan sonra yönlendirme yapılacak
+        }, 3000) 
     }
 
-    // Arka planın alfa animasyonu
+    
     val alphaAnimation = remember { Animatable(0.3f) }
 
-    // Animasyonu başlatmak için bir etki (LaunchedEffect)
+    
     LaunchedEffect(Unit) {
-        // 1 saniye içinde soluklaşacak şekilde animasyonu başlat
+        
         alphaAnimation.animateTo(
             targetValue = 1f,
             animationSpec = tween(durationMillis = 1000, easing = LinearEasing)
         )
     }
 
-    // Box ile tüm öğeleri saralım
+    
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        // Arka plan görseli, alfa animasyonu uygulanacak
+        
         Image(
-            painter = painterResource(id = R.drawable.harrypotter), // Harry Potter arka plan görseli
+            painter = painterResource(id = R.drawable.harrypotter),
             contentDescription = "Harry Potter Background",
             modifier = Modifier
                 .fillMaxSize()
-                .alpha(alphaAnimation.value), // Animasyonlu opaklık
+                .alpha(alphaAnimation.value), 
             contentScale = ContentScale.Crop
         )
     }
